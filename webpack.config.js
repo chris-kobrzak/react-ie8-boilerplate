1f0d1eaeeadaf5d96624ed1e03c0a2eea6c48538
Chris Kobrzak <chris.kobrzak@gmail.com>
2016-03-28 18:33:36 +0100

    Add production bundle NPM task (compatible with IE8)
    Commands:
    
    ```
    npm i -D babel-plugin-transform-es3-member-expression-literals \
    babel-plugin-transform-es3-property-literals
    npm i -D webpack-merge
    npm i -D local-web-server
    ```

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var path = require("path")
 var webpack = require("webpack")
+var mergeWebpackConfig = require("webpack-merge")
 
-var config = {
+var TARGET = process.env.npm_lifecycle_event
+
+var commonConfig = {
   entry: [
-    "webpack-dev-server/client?http://localhost:3200",
-    "webpack/hot/only-dev-server",
     "./src/main.jsx"
   ],
 
@@ -18,15 +19,35 @@ var config = {
     loaders: [{
       test: /\.jsx?/,
       exclude: /node\_modules/,
-      loaders: [ "react-hot", "babel" ]
+      loaders: ["babel"]
     }]
   },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-
-  devtool: "source-map"
 }
 
-module.exports = config
+var devConfig = {}
+if ( TARGET === "start" || ! TARGET ) {
+  devConfig = {
+    entry: [
+      "webpack-dev-server/client?http://localhost:3200",
+      "webpack/hot/only-dev-server",
+      "./src/main.jsx"
+    ],
+
+    module: {
+      loaders: [{
+        test: /\.jsx?/,
+        exclude: /node\_modules/,
+        loaders: ["react-hot", "babel"]
+      }]
+    },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
+
+    devtool: "source-map"
+  }
+}
+
+module.exports = mergeWebpackConfig(commonConfig, devConfig)
