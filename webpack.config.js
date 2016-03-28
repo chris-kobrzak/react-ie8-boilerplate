817b8d3ec327a9349b02123729aa68ffadf8f2f5
Chris Kobrzak <chris.kobrzak@gmail.com>
2016-03-28 20:54:22 +0100

    Add CSS support
    In the development mode we let Webpack bundle the CSS code with JavaScript and
    for "demo" and production targets, a separate stylesheet file gets created.
    
    ES5 shim and sham as well as HTML5 shiv have been added to make sure demo and
    production versions of the application can be run in IE8. Please note, it is
    not currently possible to do it for the devevelopment version, partly because
    Hot Module Loader breaks IE8.

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
 var path = require("path")
 var webpack = require("webpack")
 var mergeWebpackConfig = require("webpack-merge")
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 var TARGET = process.env.npm_lifecycle_event
+
+var config = {}
 
 var commonConfig = {
   entry: [
     "./src/main.jsx"
   ],
-
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
     publicPath: "/static/"
   },
-
   module: {
     loaders: [{
       test: /\.jsx?/,
       exclude: /node\_modules/,
       loaders: ["babel"]
     }]
-  },
-
+  }
 }
 
-var devConfig = {}
-if ( TARGET === "start" || ! TARGET ) {
-  devConfig = {
+if (TARGET === "dev:start" || ! TARGET) {
+  config = {
     entry: [
       "webpack-dev-server/client?http://localhost:3200",
       "webpack/hot/only-dev-server",
       "./src/main.jsx"
     ],
-
     module: {
       loaders: [{
         test: /\.jsx?/,
         exclude: /node\_modules/,
         loaders: ["react-hot", "babel"]
+      }, {
+        test: /\.css$/,
+        loaders: ["style", "css"],
+        include: path.join(__dirname, "src")
       }]
     },
-
     plugins: [
       new webpack.HotModuleReplacementPlugin()
     ],
@@ -50,4 +51,25 @@ if ( TARGET === "start" || ! TARGET ) {
   }
 }
 
-module.exports = mergeWebpackConfig(commonConfig, devConfig)
+if (TARGET === "demo:start" || TARGET === "prod:start"
+ || TARGET === "demo:build" || TARGET === "prod:build" || TARGET === "build") {
+  config = {
+    module: {
+      loaders: [{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style", "css"),
+        include: path.join(__dirname, "src")
+      }]
+    },
+    plugins: [
+      // Output extracted CSS to a file
+      new ExtractTextPlugin("styles.css")
+    ]
+  }
+
+  if (TARGET.match(/^demo.*/gi)) {
+    config.devtool = "source-map"
+  }
+}
+
+module.exports = mergeWebpackConfig(commonConfig, config)
